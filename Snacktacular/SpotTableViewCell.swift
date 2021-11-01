a855cebd0f736ceaca61603c8e67c78a0bfe8abb
Chloe Chen <chloechen408@gmail.com>
2021-10-31 22:42:30 -0400

    8.3 done

---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Chloe Chen on 10/31/21.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
